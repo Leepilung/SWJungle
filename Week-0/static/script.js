daf1b089a2fbfe3ef138c0e2453ea963bd802a45
Lee Pil Ung <ek3434@naver.com>
2021-11-02 17:04:53 +0900

    스크립트

---
+++ Week-0/static/script.js
@@ -0,0 +1,68 @@
+(() => {
+  const customFetch = (url, option = {}) => {
+    const useApplicationHeader = ["POST", "PUT"];
+    return fetch(url, {
+      ...option,
+      headers: {
+        ...(useApplicationHeader.includes(option.method) && {
+          "Content-Type": "application/json",
+        }),
+        ...option.headers,
+      },
+      ...(option.body && {
+        body: JSON.stringify(option.body),
+      }),
+    }).then((res) => res.json());
+  };
+
+  class Self {
+    self = [];
+
+    init() {
+      this.$postBox = document.querySelector("#post-box");
+      this.initEvent();
+    }
+
+    initEvent() {
+      document.addEventListener("click", this.handleClickEvents);
+      this.$postBox.addEventListener("submit", this.handleSubmitEvents);
+    }
+
+    handleClickEvents = (event) => {
+      const { target } = event;
+      console.log({ target });
+      const { role } = target.dataset;
+      console.log({ role });
+      switch (role) {
+        case "toggle-box":
+          this.togglePostBox();
+          break;
+        case "join":
+          window.loaction.href = "http://172.17.37.172:5000/join";
+          break;
+        case "joinSuccess":
+          console.log("가입성공");
+          break;
+        default:
+        // 아무것도 안함
+      }
+    };
+
+    togglePostBox() {
+      console.log("togglePostBox 실행");
+      this.$postBox.classList.toggle("hide");
+    }
+
+    handleSubmitEvents = (event) => {
+      event.preventDefault();
+      const { target } = event;
+      const formData = new FormData(target);
+      console.log({ target }, formData);
+      console.log(formData.get("id"), formData.get("password"));
+    };
+  }
+  const self = new Self();
+  window.onload = () => {
+    self.init();
+  };
+})();
